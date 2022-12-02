a4a4f4b1e2f57f08b0b256756be8fcdb70278562
donggyu <leedong1848@gmail.com>
2022-12-02 23:48:55 +0900

    Add: swagger 문서

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Osaka Blues Blog')
+    .setDescription('간단한 블로그를 만들어보자')
+    .setVersion('1.0')
+    .build();
+  const documet = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documet);
   await app.listen(3000);
 }
 bootstrap();
