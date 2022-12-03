14fe7794c7205b8c29af13470f6cbb36329cc9e2
donggyu <leedong1848@gmail.com>
2022-12-04 00:23:53 +0900

    temp

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PostModule } from './post/post.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+      AppModule,
+    );
   const config = new DocumentBuilder()
     .setTitle('Osaka Blues Blog')
     .setDescription('간단한 블로그를 만들어보자')
     .setVersion('1.0')
     .build();
   const documet = SwaggerModule.createDocument(app, config);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api')
   SwaggerModule.setup('docs', app, documet);
-  await app.listen(3000);
+  await app.listen(3000);``
 }
 bootstrap();
