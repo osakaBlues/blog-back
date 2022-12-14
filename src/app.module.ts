38d1e9d700e89fe08591711212e544859f0ab5a5
Seongil Yoon <books13456@gmail.com>
2022-12-14 21:23:51 +0900

    :bug: delete typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 13306,
-      username: 'root',
-      password: 'root!',
-      database: 'test',
-      models: [],
-    }),
     PostModule,
   ],
   controllers: [],
