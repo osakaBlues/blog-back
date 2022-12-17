7116ffadadd09a700d17e9e4a4351b310547b47d
donggyu <leedong1848@gmail.com>
2022-12-18 01:20:48 +0900

    fix test

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -9,6 +9,13 @@ test('test.spec for test', () => {
   expect(1 + 1).not.toBe(1);
 });
 test('test for database conneciton', async () => {
+  expect(process.env.DATABASE_TYPE).toBe('mysql');
+  expect(process.env.DATABASE_HOST).toBe('localhost');
+  expect(process.env.DATABASE_PORT).toBe('3306');
+  expect(process.env.DATABASE_USER_NAME).toBe('user');
+  expect(process.env.DATABASE_PASSWORD).toBe('test');
+  expect(process.env.DATABASE_DATABASE_NAME).toBe('testing');
+
   const AppDataSource = new DataSource({
     type: process.env.DATABASE_TYPE as MysqlConnectionOptions['type'],
     host: process.env.DATABASE_HOST,
