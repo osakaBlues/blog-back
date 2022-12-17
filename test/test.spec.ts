a60b2cdc394e35c17686d60f7d5b6841467265b5
Seongil Yoon <books13456@gmail.com>
2022-12-18 00:55:28 +0900

    :white_check_mark: add database test code

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,4 +1,23 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import type { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+dotenv.config();
+
 test('test.spec for test', () => {
   expect(1 + 1).toBe(2);
   expect(1 + 1).not.toBe(1);
+});
+test('test for database conneciton', async () => {
+  const AppDataSource = new DataSource({
+    type: process.env.DATABASE_TYPE as MysqlConnectionOptions['type'],
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USER_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE_NAME,
+  });
+  await expect(
+    AppDataSource.initialize().then(() => AppDataSource.destroy()),
+  ).resolves.not.toThrow();
 });
